9dab06f5935fc0c849ad085fb18ee2492ad0be28
jjnenonen <118576125+jjnenonen@users.noreply.github.com>
2023-02-03 19:02:54 +0200

    exercises 1.12. - 1.14.
    anectodes

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,84 +1,18 @@
-import {useState} from 'react'
+import Note from './components/Note'
 
-const Button = ({ handleClick, text }) => (
-    <button onClick={handleClick}> {text} </button>
-)
-  
-const StatisticLine = ({ name, value, end }) => ( 
-  <div>{name} {value} {end}</div> 
-  )
-
-const Statistics = (props) => {
-  //console.log(props)
-  if (props.all === 0) {
-    return (
-      <div>No feedback given</div>
-    )
-  }
-  return (
-     <table>
-      <tbody>
-        <tr>
-          <td><StatisticLine name='good' /></td>
-          <td><StatisticLine value={props.good} /></td>
-        </tr>
-        <tr>
-          <td><StatisticLine name ='neutral' /></td>
-          <td><StatisticLine value={props.neutral} /></td>
-        </tr>
-        <tr>
-          <td><StatisticLine name ='bad' /></td>
-          <td><StatisticLine value={props.bad} /></td>
-        </tr>
-        <tr>
-          <td><StatisticLine name='all' /></td>
-          <td><StatisticLine value={props.all} /></td>
-        </tr>
-        <tr>
-          <td><StatisticLine name='average' /></td>
-          <td><StatisticLine value={props.average/props.all} /></td>
-        </tr>
-        <tr>
-          <td><StatisticLine name='positive' /></td>
-          <td><StatisticLine value={(props.good/props.all)*100} end='%' /></td>
-        </tr>
-      </tbody>
-    </table>
-  )
-}
-
-const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-  const [average, setAverage] = useState(0)
-
-  const handleGood = () => {
-    setGood(good+1)
-    setAll(all+1)
-    setAverage(average+1)
-  }
-  const handleNeutral = () => {
-    setNeutral(neutral+1)
-    setAll(all+1)
-  }
-  const handleBad = () => {
-    setBad(bad+1)
-    setAll(all+1)
-    setAverage(average-1)
-  }
+const App = (props) => {
+  const { notes } = props
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <Button handleClick={handleGood} text='good' />
-      <Button handleClick={handleNeutral} text ='neutral' />
-      <Button handleClick={handleBad} text='bad' />
-      <h1>statistics</h1>
-      <Statistics  all={all} good={good} neutral={neutral} bad={bad} average={average} />
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note =>
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
     </div>
   )
 }
 
-export default App;
+export default App
